1c8511d95cc337d79bdd59bf5f9a2960c949787e
nsunilin <nsunilin@gmail.com>
2016-02-01 16:24:49 -0600

    This is second commit

---
+++ hello.py
@@ -0,0 +1,3 @@
+import sys  # imports the system libraries or system module in this program, so that we can use its methods
+print "Hello world"  # To print a statement or value
+sys.exit()  #We are using system exit function to terminate the program or lets say exit
